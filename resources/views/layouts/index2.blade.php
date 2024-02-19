55567157aee0e572b138f176f69862747136e018
Raul Ramirez Mendoza <raulramirezmendoza@alumno.ieselrincon.es>
2024-02-19 12:13:15 +0000

    Solucionando problemas

--- resources/views/layouts/index2.blade.php
+++ resources/views/layouts/index2.blade.php
@@ -5,40 +5,23 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="{{asset('/css/index.css')}}">
-    <title>mapa</title>
+    <title>Electrico</title>
 </head>
 
-    <body>
-<script type="text/javascript">
-
-    let canvasLightning = function(c, cw, ch){
-
-    this.init = function(){
-    this.loop();
-    };    
-    <body class="bodyElect">
-   
-   
-
-
-<div id="container" touch-action="none"></div>
-
-
-
+<body>
     <header class="headerElect">
+
         <h1 class="tituloElect">SmartEcoSchool</h1>
     </header>
 
-    <canvas id="Rayos"></canvas>
     <main class="main">
         <div class="center">
             <h1>@yield('graphTitle')</h1>
             @yield('text')
         </div>
-    
-    
 
         <div class="two-graphs">
+
         </div>
 
     </main>
